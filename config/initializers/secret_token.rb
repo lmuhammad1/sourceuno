55267d7e55c9f9723c2ba5324c6c5ef70e05f052
Louis Muhammad <louis.muhammad@gmail.com>
2013-03-10 00:11:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sourceuno::Application.config.secret_token = '0e5dab35389c70b7b302a3275d6d87f21de60ffcb3b62836ccd933e3c5a7ea7b6544a4f60ba387e3c22b54da472aa7d061c3914b584721f5014c9eb3c7e02025'
