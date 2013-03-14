aa9ef19f0548f443d91f36399c4b4225c387ced3
Louis Muhammad <louis.muhammad@gmail.com>
2013-03-14 17:37:20 -0400

    Added bootstrap layout and created employer page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Sourceuno::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+  get "static_pages/employer"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
