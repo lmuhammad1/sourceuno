998b6e214c4955d2ef21d75e73a660ffbfb3a87c
Louis Muhammad <louis.muhammad@gmail.com>
2013-03-17 03:00:05 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Louis Muhammad"
-    email    "louis@sourceuno.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@sourceuno.com"}   
     password "root62"
     password_confirmation "root62"
+
+    factory :admin do
+      admin true
+    end
   end
 end
