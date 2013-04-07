bfb4cf30bd1cd67fd7c663f944cfefc82f39124a
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2013-04-07 18:52:23 -0400

    Add seed data and add missing properties to models

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,3 +1,4 @@
 class Expert < ActiveRecord::Base
+  has_many :videos
   attr_accessible :first_name, :last_name
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,5 @@
 class Field < ActiveRecord::Base
   belongs_to :profession
   has_and_belongs_to_many :videos
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :profession
 end
