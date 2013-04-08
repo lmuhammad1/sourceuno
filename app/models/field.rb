06a1dff04ed100221fd54685a7f2258fd51e6863
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2013-04-07 20:21:13 -0400

    A field has_many :questions, through: :videos

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,6 @@
 class Field < ActiveRecord::Base
   belongs_to :profession
   has_and_belongs_to_many :videos
+  has_many :questions, through: :videos
   attr_accessible :description, :name, :profession
 end
